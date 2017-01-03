2957920d9cb4650a5882d06160e37c388e0790b8
qiye <594675898@qq.com>
2017-01-03 11:11:41 +0800

    Revert "修改代理成功判断逻辑，和失败次数逻辑"

--- IPProxyPool_py2/IPProxy.py
+++ IPProxyPool_py2/IPProxy.py
@@ -1,31 +1,31 @@
-#!/usr/bin/env python
-# coding:utf-8
-import sys
+#coding:utf-8
 from multiprocessing import Value, Queue, Process
-
 from api.apiServer import start_api_server
+import sys
 from db.DataStore import store_data
 from spider.ProxyCrawl import startProxyCrawl
 from validator.Validator import validator
-
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-def run():
-    DB_PROXY_NUM = Value('i', 0)
-    FAILD_IP_NUM = Value('i', 0)
+
+
+if __name__=="__main__":
+    DB_PROXY_NUM=Value('i',0)
     q1 = Queue()
     q2 = Queue()
     p0 = Process(target=start_api_server)
-    p1 = Process(target=startProxyCrawl, args=(q1, DB_PROXY_NUM))
-    p2 = Process(target=validator, args=(q1, q2, FAILD_IP_NUM))
-    p3 = Process(target=store_data, args=(q2, DB_PROXY_NUM, FAILD_IP_NUM))
+    p1 = Process(target=startProxyCrawl,args=(q1,DB_PROXY_NUM))
+    p2 = Process(target=validator,args=(q1,q2))
+    p3 = Process(target=store_data,args=(q2,DB_PROXY_NUM))
     p0.start()
     p1.start()
     p2.start()
     p3.start()
 
 
-if __name__ == "__main__":
-    run()
+
+
+
+
--- IPProxyPool_py2/db/DataStore.py
+++ IPProxyPool_py2/db/DataStore.py
@@ -1,7 +1,8 @@
-# coding: utf-8
+#coding:utf-8
 import sys
 from config import DB_CONFIG
 from util.exception import Con_DB_Fail
+
 
 try:
     if DB_CONFIG['DB_CONNECT_TYPE'] == 'pymongo':
@@ -10,29 +11,39 @@ try:
         from db.SqlHelper import SqlHelper as SqlHelper
     sqlhelper = SqlHelper()
     sqlhelper.init_db()
-except Exception, e:
+except Exception,e:
     raise Con_DB_Fail
 
 
-def store_data(useful_proxys_queue, db_proxy_num, faild_ip_num):
+def store_data(queue2,db_proxy_num):
     '''
     读取队列中的数据，写入数据库中
-    :param useful_proxys_queue:
+    :param queue2:
     :return:
     '''
     successNum = 0
+    failNum = 0
     while True:
-        if db_proxy_num.value:
-            successNum = db_proxy_num.value
-            db_proxy_num.value = 0
         try:
-            proxy = useful_proxys_queue.get(timeout=3000)
+            proxy = queue2.get(timeout=300)
             if proxy:
+
                 sqlhelper.insert(proxy)
                 successNum += 1
-            str = u'IPProxyPool----->>>>>>>>Success ip num :%d,Fail ip num:%d' % (
-                successNum, faild_ip_num.value)
-            sys.stdout.write(str + "\r")
+            else:
+                failNum += 1
+            str = u'IPProxyPool----->>>>>>>>Success ip num :%d,Fail ip num:%d' % (successNum,failNum)
+            sys.stdout.write(str+"\r")
             sys.stdout.flush()
-        except BaseException, e:
-            continue
+        except BaseException,e:
+
+            if db_proxy_num.value != 0:
+                successNum += db_proxy_num.value
+                db_proxy_num.value=0
+                str = u'IPProxyPool----->>>>>>>>Success ip num :%d,Fail ip num:%d' % (successNum,failNum)
+                sys.stdout.write(str+"\r")
+                sys.stdout.flush()
+                successNum = 0
+                failNum = 0
+
+
