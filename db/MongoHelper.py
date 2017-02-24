393dfe2f798615485ecb7460c9b5fea4e7a2304c
lightning1141 <lightning1141@gmail.com>
2017-02-24 15:28:12 +0800

    don't connect mongo at set up

--- db/MongoHelper.py
+++ db/MongoHelper.py
@@ -6,16 +6,14 @@ from db.ISqlHelper import ISqlHelper
 
 class MongoHelper(ISqlHelper):
     def __init__(self):
-        self.client = pymongo.MongoClient(DB_CONFIG['DB_CONNECT_STRING'])
+        self.client = pymongo.MongoClient(DB_CONFIG['DB_CONNECT_STRING'], connect=False)
 
     def init_db(self):
         self.db = self.client.proxy
         self.proxys = self.db.proxys
 
-
     def drop_db(self):
         self.client.drop_database(self.db)
-
 
     def insert(self, value=None):
         if value:
@@ -24,14 +22,12 @@ class MongoHelper(ISqlHelper):
                          area=value['area'], speed=value['speed'], score=0)
             self.proxys.insert(proxy)
 
-
     def delete(self, conditions=None):
         if conditions:
             self.proxys.remove(conditions)
             return ('deleteNum', 'ok')
         else:
             return ('deleteNum', 'None')
-
 
     def update(self, conditions=None, value=None):
         # update({"UserName":"libing"},{"$set":{"Email":"libing@126.com","Password":"123"}})
