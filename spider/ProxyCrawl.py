99b5a246a3b62b9b1396b3682e5666a1d5532da8
xsren <bestrenxs@gmail.com>
2017-04-13 15:06:35 +0800

    limit speed and save memory

--- spider/ProxyCrawl.py
+++ spider/ProxyCrawl.py
@@ -47,6 +47,9 @@ class ProxyCrawl(object):
             spawns = []
             for proxy in proxylist:
                 spawns.append(gevent.spawn(detect_from_db, self.myip, proxy, self.proxies))
+                if len(spawns) >= 30:
+                    gevent.joinall(spawns)
+                    spawns= []
             gevent.joinall(spawns)
             self.db_proxy_num.value = len(self.proxies)
             str = 'IPProxyPool----->>>>>>>>db exists ip:%d' % len(self.proxies)
@@ -55,7 +58,9 @@ class ProxyCrawl(object):
                 str += '\r\nIPProxyPool----->>>>>>>>now ip num < MINNUM,start crawling...'
                 sys.stdout.write(str + "\r\n")
                 sys.stdout.flush()
-                self.crawl_pool.map(self.crawl, parserList)
+                # self.crawl_pool.map(self.crawl, parserList)
+                for p in parserList:
+                    self.crawl(p)
             else:
                 str += '\r\nIPProxyPool----->>>>>>>>now ip num meet the requirement,wait UPDATE_TIME...'
                 sys.stdout.write(str + "\r\n")
